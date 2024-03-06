7dc947b174caacc8c53f503f236f73afa087201b
wannes <wannes.vdc@hotmail.com>
2024-03-06 18:24:58 +0100

    Spring Boot project initialization

---
+++ card-games/src/main/java/com/wannesvdc/games/cardgames/CardGamesApplication.java
@@ -0,0 +1,13 @@
+package com.wannesvdc.games.cardgames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardGamesApplication.class, args);
+	}
+
+}
